948b2183136f79bd2d270f5928cf400a8b9698e2
Usuario <Usuario@CPD03-M10>
2024-03-26 11:32:19 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Insira um número para a tabuada: ");
+        int numero = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Insira a quantidade de vezes desejada: ");
+        int quantidade = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine($"Tabuada do número {numero}:");
+
+        for (int i = 1; i <= quantidade; i++)
+        {
+            Console.WriteLine($"{numero} x {i} = {numero * i}");
+        }
+
+        Console.ReadKey();
+    }
+}
